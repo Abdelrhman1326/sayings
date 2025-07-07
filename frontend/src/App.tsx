bfe7aab86e986597db13ad321ee92fee7ba88b56
Abdelrhman1326 <abdelrhman132006@gmail.com>
2025-07-07 19:49:41 +0300

    all basic setup done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
+
+import Login from './components/Login';
 
 function App() {
-
   return (
-    <>
-      <p>Hello world</p>
-    </>
-  )
+    <Routes>
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,14 @@
- /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
-   content: ["./src/**/*.{html,js}"],
-   theme: {
-     extend: {},
-   },
-   plugins: [],
- }
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        jsMath: ['jsMath-cmbx10', 'serif'],
+        ibm: ['"IBM Plex Sans"', 'sans-serif'],
+        kalnia: ['Kalnia', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
