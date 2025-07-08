796d8f0d6bba9af1764d415996145ebb373ea386
Abdelrhman1326 <abdelrhman132006@gmail.com>
2025-07-08 17:27:11 +0300

    homepage left-hand-side almost done

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
         <p className="cursor-pointer">Saved</p>
       </nav>
       <div className="pr-32 absolute right-4">
-        <Button text="Sign in" className="font-kalnia font-bold rounded-full w-[125px] h-[45px]
+        <Button text="Sign in" className="font-jsMath font-bold rounded-full w-[125px] h-[45px]
         border-solid border-white border-4 text-[22px] flex items-center justify-center" />
       </div>
     </div>
--- frontend/src/components/subComponents/HomeBody.tsx
+++ frontend/src/components/subComponents/HomeBody.tsx
@@ -1,18 +1,23 @@
+import Button from "../ui/Button";
+
 const HomeBody = () => {
   return (
-    <div className="pl-32 pr-32 pt-12">
+    <div className="pl-32 pr-32 pt-24 cursor-default">
         <h1 className="text-white font-jsMath font-bold text-[90px] mt-12 leading-[1]">
             <span className="block">Discover</span>
             <span className="block mt-2">Hidden Lines</span>
         </h1>
-        <div className="pr-[800px] mt-12">
+        <div className="pr-[800px] mt-8">
             <h2 className="text-white font-ibm font-bold text-[24px]">
                 Enjoy reading handpicked quotes from books—created, edited, and
                 ranked by a community of passionate readers like you.
             </h2>
         </div>
+        <div className="mt-12 items-center relative">
+            <Button text={"Get A Quote"} className="font-jsMath font-bold text-[30px] rounded-full border-solid border-white border-4" />
+        </div>
     </div>
   )
 }
 
-export default HomeBody
+export default HomeBody;
