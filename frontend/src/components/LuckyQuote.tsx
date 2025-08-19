083bf58884a21b7c0bd36de3cf2cdab92b9e6987
AbdElrhman <abdelrhman132006@gmail.com>
2025-08-19 17:47:41 +0300

    working on lucky quote page

--- frontend/src/components/LuckyQuote.tsx
+++ frontend/src/components/LuckyQuote.tsx
@@ -1,7 +1,10 @@
+import Logo from "./ui/Logo";
+
 const LuckyQuote = () => {
   return (
-    <div className="text-white">
-      Lucky Quote
+    <div className="flex place-items-center gap-24 w-screen pl-32 pr-32 pt-8 bg-[#141414] bg-opacity-95">
+      <Logo size={44} />
+      <p className="text-uiPrimary font-bold font-ibm mt-2 text-[16px]">Lucky Quote</p>
     </div>
   )
 }
